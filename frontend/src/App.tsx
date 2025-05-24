e724e018e6b4dfdd512af72fe6c88516afa32cc9
Maximilian Alvim de Faria <maadf@kth.se>
2025-05-24 14:18:04 +0200

    Debug message for vercel frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import Home from './pages/Home';
 import { ProtectedRoute } from './components/ProtectedRoute';
 
 function App() {
+  console.log('VITE_API_BASE_URL in Vercel build:', import.meta.env.VITE_API_BASE_URL);
+
   return (
     <Router>
       <Routes>
